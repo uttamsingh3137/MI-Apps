07ff005eb49ca83bc99ddddb1e5302faf987aa98
uttam pratap <uttampratp9871@gmail.com>
2025-12-02 12:03:13 +0530

    mi

--- MI app/mi-app/src/api/postApi.ts
+++
@@ -1,7 +0,0 @@
-import type { Post } from "../types/postTypes";
-import { api } from "./apiClients";
-
-export const fetchPosts = async() : Promise<Post[]> =>{
-    const res = await api.get("/posts");
-    re
-}
